4fa40c8235a58b6e771f73432a57278ce2f5dc55
Valeriya <109979423+BernikovaLera@users.noreply.github.com>
2024-04-22 20:26:51 +0300

    Update homework.js

--- js/05mod-oop/homework.js
+++ js/05mod-oop/homework.js
@@ -7,15 +7,15 @@
 названий свойств будут их значения.
 Создайте переменную c1 с городами Екатеринбург и Москва
 */
-// function Card(from, to) {
-//     this.from = from;
-//     this.to = to;
-// }
-// Card.prototype.show = function() {
-//     return this.from + ', ' + this.to;
-// };
-// var c1 = new Card('Екатеринбург', 'Москва');
-// console.log(c1.show()); 
+function Card(from, to) {
+    this.from = from;
+    this.to = to;
+}
+Card.prototype.show = function() {
+    return this.from + ', ' + this.to;
+};
+var c1 = new Card('Екатеринбург', 'Москва');
+console.log(c1.show()); 
 
 
 // Задание 2
@@ -39,33 +39,33 @@
 метод геттер firstname ( он должен возвращать
 свойство name объекта)
 */ 
-// class Human {
-//     constructor(name, age, height) {
-//         this.name = name;
-//         this.age = age;
-//         this.height = height;
-//     }
-//     getInfo() {
-//         return `${this.name}, ${this.age}, ${this.height}`;
-//     }
-//     getFirstName() {
-//         return this.name;
-//     }
-// }
-// const humans = [
-//     new Human('Коля', 23, 180),
-//     new Human('Даша', 19, 170),
-//     new Human('Ваня', 18, 192),
-//     new Human('Петя', 45, 178),
-//     new Human('Вася', 34, 197),
-//     new Human('Джони', 40, 168),
-//     new Human('Катя', 37, 160),
-//     new Human('Петя', 29, 200),
-//     new Human('Соня', 21, 172),
-//     new Human('Женя', 25, 175)
-// ];
-// console.log(humans[0].getInfo()); 
-// console.log(humans[0].getFirstName()); 
+class Human {
+    constructor(name, age, height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+    getInfo() {
+        return `${this.name}, ${this.age}, ${this.height}`;
+    }
+    getFirstName() {
+        return this.name;
+    }
+}
+const humans = [
+    new Human('Коля', 23, 180),
+    new Human('Даша', 19, 170),
+    new Human('Ваня', 18, 192),
+    new Human('Петя', 45, 178),
+    new Human('Вася', 34, 197),
+    new Human('Джони', 40, 168),
+    new Human('Катя', 37, 160),
+    new Human('Петя', 29, 200),
+    new Human('Соня', 21, 172),
+    new Human('Женя', 25, 175)
+];
+console.log(humans[0].getInfo()); 
+console.log(humans[0].getFirstName()); 
 
 
 // Задание 3 (*) 
@@ -76,27 +76,27 @@
 росту (по убыванию роста). Вызовите только 
 функцию сортировки по росту
 */
-// function sortByName(array) {
-//     return array.sort((a, b) => {
-//         if (a.name < b.name) return -1;
-//         if (a.name > b.name) return 1;
-//         return 0;
-//     });
-// }
-// function sortByHeight(array) {
-//     return array.sort((a, b) => {
-//         if (a.height < b.height) return 1;
-//         if (a.height > b.height) return -1;
-//         return 0;
-//     });
-// }
-// const people = [
-//     { name: 'Alice', height: 160 },
-//     { name: 'Bob', height: 180 },
-//     { name: 'Charlie', height: 170 },
-// ];
-// const sortedByHeight = sortByHeight(people);
-// console.log(sortedByHeight);
+function sortByName(array) {
+    return array.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+    });
+}
+function sortByHeight(array) {
+    return array.sort((a, b) => {
+        if (a.height < b.height) return 1;
+        if (a.height > b.height) return -1;
+        return 0;
+    });
+}
+const people = [
+    { name: 'Alice', height: 160 },
+    { name: 'Bob', height: 180 },
+    { name: 'Charlie', height: 170 },
+];
+const sortedByHeight = sortByHeight(people);
+console.log(sortedByHeight);
 
 // Задание 4
 /* 
@@ -104,8 +104,8 @@
 соответствующую 0:00, 1 января 2045 года,
 при помощи конструктора Date()
 */
-// var dt1 = new Date(2045, 0, 1);
-// console.log(dt1);
+var dt1 = new Date(2045, 0, 1);
+console.log(dt1);
 
 // Задание 5
 /* 
@@ -113,11 +113,11 @@
 которое прошло с 1 января 1970 года по 
 текущий момент.
  */
-// var currentDate = new Date();
-// var millisecondsSinceUnixEpoch = currentDate.getTime();
-// var secondsSinceUnixEpoch = millisecondsSinceUnixEpoch / 1000;
-// var dt2 = secondsSinceUnixEpoch;
-// console.log(dt2);
+var currentDate = new Date();
+var millisecondsSinceUnixEpoch = currentDate.getTime();
+var secondsSinceUnixEpoch = millisecondsSinceUnixEpoch / 1000;
+var dt2 = secondsSinceUnixEpoch;
+console.log(dt2);
 
 // Задание 6 (*)
 /* 
@@ -126,17 +126,17 @@
 февраль — 2 и т.д.), а возвращает количество
 дней в месяце
  */
-// function getDays(year, month) {
-//     var isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-//     var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-//     if (isLeapYear) {
-//         daysInMonth[1] = 29;
-//     }
-//     return daysInMonth[month - 1];
-// }
-// console.log(getDays(2023, 1)); 
-// console.log(getDays(2020, 2)); 
-// console.log(getDays(2021, 2)); 
+function getDays(year, month) {
+    var isLeapYear = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    if (isLeapYear) {
+        daysInMonth[1] = 29;
+    }
+    return daysInMonth[month - 1];
+}
+console.log(getDays(2023, 1)); 
+console.log(getDays(2020, 2)); 
+console.log(getDays(2021, 2)); 
 
 
 
@@ -147,31 +147,31 @@
 месяца и год, а возвращает разметку
 календаря HTML на указанный месяц.
 */
-// function calendar(year, month) {
-//     function getDaysInMonth(month, year) {
-//         return new Date(year, month, 0).getDate();
-//     }
-//     const daysInMonth = getDaysInMonth(month, year);
-//     const calendarHTML = `
-//         <table>
-//             <tr>
-//                 <th>Пн</th>
-//                 <th>Вт</th>
-//                 <th>Ср</th>
-//                 <th>Чт</th>
-//                 <th>Пт</th>
-//                 <th>Сб</th>
-//                 <th>Вс</th>
-//             </tr>
-//             <tr>
-//                 <td>${daysInMonth}</td>
-//             </tr>
-//         </table>
-//     `;
-//     return calendarHTML;
-// }
-// const calendarHTML = calendar(2023, 1);
-// console.log(calendarHTML);
+function calendar(year, month) {
+    function getDaysInMonth(month, year) {
+        return new Date(year, month, 0).getDate();
+    }
+    const daysInMonth = getDaysInMonth(month, year);
+    const calendarHTML = `
+        <table>
+            <tr>
+                <th>Пн</th>
+                <th>Вт</th>
+                <th>Ср</th>
+                <th>Чт</th>
+                <th>Пт</th>
+                <th>Сб</th>
+                <th>Вс</th>
+            </tr>
+            <tr>
+                <td>${daysInMonth}</td>
+            </tr>
+        </table>
+    `;
+    return calendarHTML;
+}
+const calendarHTML = calendar(2023, 1);
+console.log(calendarHTML);
 
 
 // Задание 8 (*)
@@ -197,38 +197,38 @@ true, если число нечетное.
 конструктор Fighter() с свойством power. 
 Примечание: не забудьте о геттерах и сеттерах.
 */
-// function Unit(x, y) {
-//     this.x = x;
-//     this.y = y;
-// }
-// Unit.prototype.getX = function() {
-//     return this.x;
-// };
-// Unit.prototype.setX = function(newX) {
-//     this.x = newX;
-// };
-// Unit.prototype.getY = function() {
-//     return this.y;
-// };
-// Unit.prototype.setY = function(newY) {
-//     this.y = newY;
-// };
-// function Fighter(x, y, power) {
-//     Unit.call(this, x, y); 
-//     this.power = power;
-// }
-// Fighter.prototype = Object.create(Unit.prototype);
-// Fighter.prototype.constructor = Fighter;
-// Fighter.prototype.getPower = function() {
-//     return this.power;
-// };
-// Fighter.prototype.setPower = function(newPower) {
-//     this.power = newPower;
-// };
-// var fighter = new Fighter(10, 10, 100);
-// console.log(fighter.getX());
-// console.log(fighter.getY()); 
-// console.log(fighter.getPower()); 
+function Unit(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Unit.prototype.getX = function() {
+    return this.x;
+};
+Unit.prototype.setX = function(newX) {
+    this.x = newX;
+};
+Unit.prototype.getY = function() {
+    return this.y;
+};
+Unit.prototype.setY = function(newY) {
+    this.y = newY;
+};
+function Fighter(x, y, power) {
+    Unit.call(this, x, y); 
+    this.power = power;
+}
+Fighter.prototype = Object.create(Unit.prototype);
+Fighter.prototype.constructor = Fighter;
+Fighter.prototype.getPower = function() {
+    return this.power;
+};
+Fighter.prototype.setPower = function(newPower) {
+    this.power = newPower;
+};
+var fighter = new Fighter(10, 10, 100);
+console.log(fighter.getX());
+console.log(fighter.getY()); 
+console.log(fighter.getPower()); 
 
 
 // Задание 11 (*)
@@ -239,37 +239,37 @@ true, если число нечетное.
 с свойством power. 
 Примечание: не забудьте о геттерах и сеттерах.
 */
-// class Unit {
-//     constructor(x, y) {
-//         this.x = x;
-//         this.y = y;
-//     }
-//     getX() {
-//         return this.x;
-//     }
-//     setX(newX) {
-//         this.x = newX;
-//     }
-//     getY() {
-//         return this.y;
-//     }
-//     setY(newY) {
-//         this.y = newY;
-//     }
-// }
-// class Fighter extends Unit {
-//     constructor(x, y, power) {
-//         super(x, y); 
-//         this.power = power;
-//     }
-//     getPower() {
-//         return this.power;
-//     }
-//     setPower(newPower) {
-//         this.power = newPower;
-//     }
-// }
-// const fighter = new Fighter(10, 10, 100);
-// console.log(fighter.getX()); 
-// console.log(fighter.getY()); 
-// console.log(fighter.getPower()); 
+class Unit {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    getX() {
+        return this.x;
+    }
+    setX(newX) {
+        this.x = newX;
+    }
+    getY() {
+        return this.y;
+    }
+    setY(newY) {
+        this.y = newY;
+    }
+}
+class Fighter extends Unit {
+    constructor(x, y, power) {
+        super(x, y); 
+        this.power = power;
+    }
+    getPower() {
+        return this.power;
+    }
+    setPower(newPower) {
+        this.power = newPower;
+    }
+}
+const fighter = new Fighter(10, 10, 100);
+console.log(fighter.getX()); 
+console.log(fighter.getY()); 
+console.log(fighter.getPower()); 
